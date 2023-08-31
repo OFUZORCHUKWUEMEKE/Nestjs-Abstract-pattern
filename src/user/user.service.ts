d82b29ae64864158fc4d93ceaea8468ca700c9ec
Ofuzor chukwuemeke <uniben2018@gmail.com>
2023-08-31 23:27:52 +0100

    added

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
-
+import { User } from './user.entity';
+import { UserRepository } from './user.repository';
+import { DeepPartial } from 'typeorm'
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(private readonly userRepository: UserRepository) { }
+    async getUsers() {
+        await this.userRepository.findAll()
+    }
+
+    async createUser(body: DeepPartial<User>) {
+       return this.userRepository.create(body)
+    }
+
+    async getUserById(id:string){
+        return this.userRepository.findOneById(id)
+    }
+}
