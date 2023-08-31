0d2437b2582d72b8a35bd016a58d62dcf65313db
Ofuzor chukwuemeke <uniben2018@gmail.com>
2023-08-31 23:25:47 +0100

    added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { User } from './user/user.entity';
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, TypeOrmModule.forRootAsync({
+    useFactory: () => {
+      return {
+        type: 'postgres',
+        url: 'postgresql://emeke:password@localhost:5434/user',
+        // autoLoadEntities: true,
+        synchronize: true,
+        host: 'localhost',
+        entities: [
+             User
+        ]
+      }
+    }
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
